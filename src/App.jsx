42e8fddee549f520904c386f95a0e2f507b4d75f
Gatadevaishnavi <gatadevaishnavi20@gmail.com>
2024-10-13 18:34:47 +0530

    code1

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import Navbar from './layout/Navbar'
+import React from "react";
+import Navbar from "./layout/Navbar";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Home from './Pages/Home';
-import { Route, BrowserRouter as Router,Routes } from 'react-router-dom';
-import AddUser from './Users/AddUser';
-import EditUser from './Users/EditUser';
+import Home from "./Pages/Home";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import AddUser from "./Users/AddUser";
+import EditUser from "./Users/EditUser";
 function App() {
   return (
     <div>
       <Router>
-      <Navbar/>
-      <Routes>
-        <Route exact path="/"element={<Home/>}></Route>
-        <Route exact path="/adduser"element={<AddUser/>}></Route>
-        <Route exact path="/edituser/:id"element={<EditUser/>}></Route>
-      </Routes>
-      
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/adduser" element={<AddUser />}></Route>
+          <Route exact path="/edituser/:id" element={<EditUser />}></Route>
+          
+
+          
+        </Routes>
       </Router>
-      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
